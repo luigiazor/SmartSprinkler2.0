06e403dc579ff44a5ca544ddf5b6fecb41053d4b
luigiazor <luigiazor@live.com>
2020-11-05 11:05:09 -0300

    Adicionar arquivos de projeto.

---
+++ SmartSprinkler/SmartSprinkler/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SmartSprinkler
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = String.IsNullOrEmpty(EmailAdressEntry.Text);
+            bool isPasswordEmpty = String.IsNullOrEmpty(PasswordEntry.Text);
+            if(isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
---
+++ SmartSprinkler/SmartSprinkler/NewPlant.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartSprinkler
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewPlant : ContentPage
+    {
+        public NewPlant()
+        {
+            InitializeComponent();
+        }
+    }
+}
